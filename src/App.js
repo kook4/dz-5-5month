5bb7930b2959848b8e51432b77ad939ce90afcd5
kook4 <talantbekov0404@gmail.com>
2022-12-14 01:12:20 +0600

    dz5

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import UsersListPage from "./pages/usersListPage/UsersListPage";
+import UserInfo from "./components/userInfo/UserInfo";
+import AddUserPage from "./pages/addUserPage/AddUserPage";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Header from "./components/header/Header";
+import {Container} from "react-bootstrap";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <Container>
+                <Header/>
+                <Routes>
+                    <Route index element={<UsersListPage/>}/>
+                    <Route path="register" element={<AddUserPage/>}/>
+
+                    <Route path="/:id" element={<UserInfo/>}/>
+                </Routes>
+            </Container>
+
+        </BrowserRouter>
+    );
 }
 
 export default App;
